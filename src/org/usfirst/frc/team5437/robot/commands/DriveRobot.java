c15c69b69b6cd8a3fc4eafdc49e5ac638d89d6a9
Caleb Hopper <calhopp@gmail.com>
2017-02-02 19:42:04 -0600

    Changed to Arcade Drive

--- src/org/usfirst/frc/team5437/robot/commands/DriveRobot.java
+++ src/org/usfirst/frc/team5437/robot/commands/DriveRobot.java
@@ -21,7 +21,7 @@ public class DriveRobot extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.chassis.Drive(-Robot.oi.getJoy1().getRawAxis(1), -Robot.oi.getJoy2().getRawAxis(1));
+    	Robot.chassis.Drive(-Robot.oi.getJoy1().getRawAxis(1), Robot.oi.getJoy1().getRawAxis(0));
     }
 
     // Make this return true when this Command no longer needs to run execute()
